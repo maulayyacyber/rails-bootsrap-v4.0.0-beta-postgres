4c25dcb1cb6369125ecf343b4134501022535896
Fika Ridaul Maulayya <ridaulmaulayya@gmail.com>
2017-08-22 22:51:03 +0700

    add about us

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   resources :blogs
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root to: 'homes#index'
+
+  get '/about', to: 'about#index'
+
 end
