d4090742e5049087e3f1afa2539b69bd35ceaaa4
Fika Ridaul Maulayya <ridaulmaulayya@gmail.com>
2017-08-22 22:24:13 +0700

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'blogs#index'
+  root to: 'homes#index'
 end
